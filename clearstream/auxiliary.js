faeb82e154ff04280c6877b1c21952f98c4faac8
KPouianou <kpouianou@gmail.com>
2013-06-23 13:41:13 +0300

    fixes to algo

--- clearstream/auxiliary.js
+++ clearstream/auxiliary.js
@@ -90,7 +90,7 @@ exports.spaceSaving =  function(linkList, linkListLength, tweet) {
     
       		if (error == null && response.statusCode == 200 && !isSpam(url, keywords.spamUrls)) {
         	  body = body.replace(scripts, "");
-        	  parser.write(body);
+        	  parser.reset();
         	  article = readable.getArticle();
         	  //remove multiple white spaces
         	  article.html = article.html.replace(/\s+/g, ' ');
